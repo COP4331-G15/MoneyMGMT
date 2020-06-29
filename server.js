1d487ebb440e340141c7519ce2dbe9f3c29fdf6d
gitebates <44310050+gitebates@users.noreply.github.com>
2020-06-28 21:26:48 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+require('./server/db-conn');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// mount routes
+app.use('/api/thoughts/', require('./server/routes/thoughts-route'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => console.log(`Wizardry happening on port ${PORT}`));
